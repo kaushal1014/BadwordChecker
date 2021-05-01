9ad7e8e09d381fb9d0279452acc107d80ddb7fa1
kaushal.1014 <patilkshl@gmail.com>
2021-05-02 00:24:36 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Chrome(executable_path="path to chrome executable")
+
+driver.maximize_window()
+driver.get("https://kaushal1014.github.io/")
+time.sleep(5)
+
+text=["ass","fuck"]
+for words in text:
+    check=words 
+
+try:
+    x=driver.find_element_by_xpath(f"//*[contains(text(),'{check}')]")
+    print("Bad word found")
+except NoSuchElementException:
+    print("No bad words found")
+driver.close()
+
