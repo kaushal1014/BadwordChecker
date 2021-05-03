180f6893e0654ae8a9d23a10f4872fd8b7646ced
kaushal.1014 <patilkshl@gmail.com>
2021-05-03 19:11:03 +0530

    url input added

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 import time
 from selenium.common.exceptions import NoSuchElementException
 
+url=input("Enter url:")
+if "https://www." not in url:
+    https=url
+else:
+    https="https://www."+url
 
 good=input("Enter the good words:")
 bad=input("Enter the bad words:")
@@ -21,7 +26,7 @@ for bad in bad_list:
 driver = webdriver.Chrome(executable_path="C:\\pythonProjects\\WordChecker - Copy\\chromedriver.exe")
 
 driver.maximize_window()
-driver.get("https://www.noswearing.com/dictionary")
+driver.get(https)
 time.sleep(5)
 
 
@@ -29,7 +34,8 @@ body=driver.find_element_by_css_selector("body")
 time.sleep(5)
 content=(body.text)
 if any(good_words in content for good_words in good_list):
-    print("good words found")
+    print(f"good words found")
+    
 else:
     print("No good words found")
 if any(bad_words in content for bad_words in bad_list):
